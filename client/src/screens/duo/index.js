56cb54bcdda5ba6cdcfd3316e20d7189b06c60d0
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-04-14 01:27:42 +0530

    completed duo page

--- client/src/screens/duo/index.js
+++ client/src/screens/duo/index.js
@@ -3,12 +3,12 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useStateProvider } from "../../utils/StateProvider";
 import "./duo.css"
+import BridgeLoad from "./BridgeLoad";
+
 
 function Duo() {
   const SearchResultCard = ({ songName, artistNames, imageUrl, onClick }) => {
     return (
-
-      
       <div
         style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
         onClick={onClick}
@@ -39,6 +39,8 @@ function Duo() {
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [songSelected, setSongSelected] = useState(false);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
     const searchTracks = async () => {
@@ -67,7 +69,7 @@ function Duo() {
           { songName, artistNames, imageUrl },
         ]);
         offset = (offset + 1) % 10;
-        await new Promise(resolve => setTimeout(resolve, 500)); 
+        await new Promise(resolve => setTimeout(resolve, 500));
       }
     };
     searchTracks();
@@ -84,75 +86,92 @@ function Duo() {
     setSongSelected(true);
   };
 
+  const handleClick = () => {
+    if (!songSelected) {
+      return;
+      
+    }
+    setLoading(true);
+  };
+
   return (
-    <div
-      className="bg-black flex flex-col justify-center items-center h-screen"
-      style={{
-        transform: "translate(-50%, -50%)",
-        position: "absolute",
-        top: "47%",
-        left: "50%",
-      }}
-    >
-      <h1 className="text-white text-4xl text-center whitespace-nowrap mb-8">
-        Select a song to recommend
-      </h1>
+    <>
       <div
-        className="relative"
-        style={{ height: "464px", width: "454px", overflow: "hidden" }}
+        className="bg-black flex flex-col justify-center items-center h-screen relative"
+        style={{
+          transform: "translate(-50%, -50%)",
+          position: "absolute",
+          top: "47%",
+          left: "50%",
+        }}
       >
-        <svg
-          className="absolute top-0 left-0 mt-2 ml-2 w-6 h-6 text-gray-600"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+        <h1 className="text-white text-4xl text-center whitespace-nowrap mb-8">
+          Select a song to recommend
+        </h1>
+        <div
+          className="relative"
+          style={{ height: "464px", width: "454px", overflow: "hidden" }}
         >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-        <input
-          type="text"
-          placeholder="Songs"
-          required
-          className="w-80 pl-10 mb-4 rounded-md border border-gray-300 p-2 text-gray-800 focus:outline-none focus:border-green-500"
-          value={searchValue}
-          onChange={handleSearch}
-          style={{ width: "454px" }}
-        />
-        <div style={{ overflowY: "scroll", height: "376px" }}>
+          <svg
+            className="absolute top-0 left-0 mt-2 ml-2 w-6 h-6 text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
+          <input
+            type="text"
+            placeholder="Songs"
+            required
+            className="w-80 pl-10 mb-4 rounded-md border border-gray-300 p-2 text-gray-800 focus:outline-none focus:border-green-500"
+            value={searchValue}
+            onChange={handleSearch}
+            style={{ width: "454px" }}
+          />
+          <div style={{ overflowY: "scroll", height: "376px" }}>
 
 
-          {searchValue !== "" &&
-            searchResults
-              .sort((a, b) => a.songName.localeCompare(b.songName))
-              .slice(0, 10)
-              .map((result, index) => (
-                <SearchResultCard
-                  key={result.id || index}
-                  songName={result.songName}
-                  artistNames={result.artistNames}
-                  imageUrl={result.imageUrl}
-                  onClick={() => {
-                   
-                    handleSongSelect(result.songName)
-                  }}
-                />
-              ))}
+            {searchValue !== "" &&
+              searchResults
+                .sort((a, b) => a.songName.localeCompare(b.songName))
+                .slice(0, 10)
+                .map((result, index) => (
+                  <SearchResultCard
+                    key={result.id || index}
+                    songName={result.songName}
+                    artistNames={result.artistNames}
+                    imageUrl={result.imageUrl}
+                    onClick={() => {
 
-              
+                      handleSongSelect(result.songName)
+                    }}
+                  />
+                ))}
+
+
+          </div>
         </div>
+        <button
+          type="match"
+          onClick={handleClick}
+          disabled={!songSelected}
+          className="bg-blue-600 hover:bg-blue-400 text-white rounded-full px-4 py-2 text-lg focus:outline-none focus:shadow-outline-blue border-white border-2"
+        >
+          Match
+        </button>
+        {loading && (
+          <div className="w-full h-full absolute">
+            <BridgeLoad/>       
+          </div>
+        )}
       </div>
-      <button
-        type="match"
-        className="bg-blue-600 hover:bg-blue-400 text-white rounded-full px-4 py-2 text-lg focus:outline-none focus:shadow-outline-blue border-white border-2"
-      >
-        Match
-      </button>
-    </div>
+    </>
   );
 }
 
