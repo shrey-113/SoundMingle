93b49e8682dff03ed1e3688e11c104fe19df3e46
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2023-04-18 14:41:14 +0530

    fixes

--- client/src/components/Duo/chatbox.js
+++ client/src/components/Duo/chatbox.js
@@ -40,23 +40,33 @@ const Chatbox = ({ currentUser, recipientUser }) => {
 
   return (
     <div className="bg-black m-20 border-solid border-white border-2 rounded max-h-96">
-      <div className="text-white m-2 font-bold">Chatting with {recipientUser}</div>
+      <div className="text-white m-2 font-bold">
+        Chatting with {recipientUser}
+      </div>
       <div className="">
         {messages.map((message, index) => (
           <div className="bg-blue-700 m-2 rounded" key={index}>
-            <div className="text-white mx-2 text-xs font-thin">{message.from}</div>
+            <div className="text-white mx-2 text-xs font-thin">
+              {message.from}
+            </div>
             <div className="text-white mx-2 ">{message.message}</div>
           </div>
         ))}
       </div>
       <form onSubmit={handleSubmit}>
-        <input className="m-2 border-solid border-white text-black border-2 rounded text-sm p-1 placeholder-black"
+        <input
+          className="m-2 border-solid border-white text-black border-2 rounded text-sm p-1 placeholder-black"
           type="text"
           value={inputMessage}
           placeholder="Enter your message"
           onChange={(event) => setInputMessage(event.target.value)}
         />
-        <button className="text-white bg-gray-500 border-white rounded text-sm p-2 mr-2" type="submit">Send</button>
+        <button
+          className="text-white bg-gray-500 border-white rounded text-sm p-2 mr-2"
+          type="submit"
+        >
+          Send
+        </button>
       </form>
     </div>
   );
